ba88b8f71e9148b7b7b88393f4cb18abab8c033a
Phong Nguyen <phong.nt.26101993@gmail.com>
2023-09-28 15:55:41 +0800

    + Update correct argurment

--- gcp/Dataproc/glamira.py
+++ gcp/Dataproc/glamira.py
@@ -7,8 +7,5 @@ rdd = sc.parallelize(['Hello,', 'world!'])
 words = sorted(rdd.collect())
 print(words)
 
-n = int(sys.argv[1])
-a = 2
-for _ in range(n):
-    print(a, sys.argv[a])
-    a += 1
+data = sys.argv[1]
+print(data)
